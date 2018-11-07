e5d78e66d1930a15f57b878b7b111f16689804bd
Ezequiel Normey <42079484+ezenormey@users.noreply.github.com>
2018-11-07 10:58:33 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -14,4 +14,4 @@ class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
 
 class Candidatos(models.Model):
-nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=200)
